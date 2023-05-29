60e5cfdf511841fadacecf8d69b108544e8132ac
Nathalie Pires Cesarino <nathalie.cesarino@kroton.tech>
2023-05-29 18:22:44 -0300

    Initial commit

---
+++ UberLocationSearch/SeachViewController.swift
@@ -0,0 +1,8 @@
+//
+//  SeachViewController.swift
+//  UberLocationSearch
+//
+//  Created by Nathalie Cesarino on 14/06/22.
+//
+
+import Foundation
--- UberLocationSearch/ViewController.swift
+++ UberLocationSearch/ViewController.swift
@@ -7,13 +7,106 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate, UITableViewDelegate, UITableViewDataSource {
 
+    private let label: UILabel = {
+        let label = UILabel()
+        label.text = "Where To?"
+        label.font = .systemFont(ofSize: 24, weight: .semibold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let field: UITextField = {
+        let field = UITextField()
+        field.placeholder = "Search destination"
+        field.layer.cornerRadius = 9
+        field.backgroundColor = .tertiarySystemBackground
+        field.translatesAutoresizingMaskIntoConstraints = false
+        return field
+    }()
+    
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        table.translatesAutoresizingMaskIntoConstraints = false
+        return table
+    }()
+    
+    var locations = [Location]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .secondarySystemBackground
+        view.addSubview(label)
+        view.addSubview(field)
+        view.addSubview(tableView)
+        setupConstraints()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.backgroundColor = .secondarySystemBackground
+        field.delegate = self
     }
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        field.resignFirstResponder()
+        if let text = field.text, !text.isEmpty {
+            LocationManager.shared.findLocations(with: text) { [weak self] locations in
+                DispatchQueue.main.async {
+                    self?.locations = locations
+                    self?.tableView.reloadData()
+                }
+            }
+        }
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return locations.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = locations[indexPath.row].title
+        cell.textLabel?.numberOfLines = 0
+        cell.contentView.backgroundColor = .secondarySystemBackground
+        cell.backgroundColor = .secondarySystemBackground
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        // Geocode location to use it later in another logic
+        let coordinate = locations[indexPath.row].coordinates
+    }
+    
+    func setupConstraints() {
+        let safeArea = view.safeAreaLayoutGuide
+        
+        NSLayoutConstraint.activate([
+            label.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 20),
+            label.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 20),
+        ])
+        
+        NSLayoutConstraint.activate([
+            field.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 20),
+            field.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 20),
+            field.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -20),
+            field.heightAnchor.constraint(equalToConstant: 50)
+        ])
+        
+        NSLayoutConstraint.activate([
+//            tableView.topAnchor.constraint(equalTo: field.bottomAnchor, constant: 5),
+            tableView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor),
+            tableView.heightAnchor.constraint(equalToConstant: view.frame.size.height),
+            tableView.widthAnchor.constraint(equalToConstant: view.frame.size.width)
+        ])
+    }
 
 }
 
